83b8c47cb7e07402292299f06a0cbd165bbf960f
MuhammedSavaad <savadmoorkkanad@gmail.com>
2023-04-03 12:07:51 +0530

    Toast Function Added!!

--- src/Components/TodoApp/TodoApp.jsx
+++ src/Components/TodoApp/TodoApp.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./TodoApp.css";
 
 class TodoApp extends Component {
@@ -22,6 +24,17 @@ class TodoApp extends Component {
       todos: allTodos,
       inputValue: "",
     });
+
+    toast.success("Todo Added 👏", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
   };
 
   deleteTodo = (id) => {
@@ -31,43 +44,68 @@ class TodoApp extends Component {
     this.setState({
       todos: allTodos,
     });
+
+    toast.info("Todo Deleted!! 👋", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
   };
 
   render() {
     const { inputValue, todos } = this.state;
 
     return (
-      <div className="todoContainer">
-        <h1>Todo App</h1>
+      <>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <div className="todoContainer">
+          <h1>Todo App</h1>
 
-        <div className="todoInput">
-          <input
-            type="text"
-            placeholder="Enter Todosss..."
-            value={inputValue}
-            onChange={this.handleChange}
-          ></input>
-          <button onClick={this.storeTodoos}>
-            <i className="bx bx-plus"></i>
-          </button>
-        </div>
+          <div className="todoInput">
+            <input
+              type="text"
+              placeholder="Enter Todosss..."
+              value={inputValue}
+              onChange={this.handleChange}
+            ></input>
+            <button onClick={this.storeTodoos}>
+              <i className="bx bx-plus"></i>
+            </button>
+          </div>
 
-        <div className="todooss">
-          <ul>
-            {todos.map((data, index) => {
-              return (
-                <div key={index}>
-                  <li>{data}</li>
-                  <i
-                    className="bx bxs-trash"
-                    onClick={() => this.deleteTodo(index)}
-                  ></i>
-                </div>
-              );
-            })}
-          </ul>
+          <div className="todooss">
+            <ul>
+              {todos.map((data, index) => {
+                return (
+                  <div key={index}>
+                    <li>{data}</li>
+                    <i
+                      className="bx bxs-trash"
+                      onClick={() => this.deleteTodo(index)}
+                    ></i>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
